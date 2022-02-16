ce297e0d6525842b02a9a06e9d071c3336021f2a
BMW90112 <99808780+BMW90112@users.noreply.github.com>
2022-02-16 20:44:47 +0700

    Add files via upload

---
+++ BM HUB auto farm Build A Boat For Treasure.lua
@@ -0,0 +1,9 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("BMW SO HANDSOME HUB", "Sentinel")
+
+local Tab = Window:NewTab("farm")
+local Section = Tab:NewSection("Build A Boat For Treasure")
+
+Section:NewButton("auto farm", "farm money so god", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BoaHacker/ROBLOX/main/cheat', true))()
+end)
